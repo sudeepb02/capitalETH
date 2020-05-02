84e2b8962498e38f975ff3c18f3d17166ef0f198
Sudeep Biswas <sudeepbiswas02@gmail.com>
2020-05-03 03:19:44 +0530

    Add Kyber Network Proxy to swap tokens - Add Kyber Network Proxy Interface - Add Kyber token swap function to main contract - Add processSingleSIP function - Restructure code

--- contracts/CapitalETH.sol
+++ contracts/CapitalETH.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.6.0;
 
+import "./ERC20Interface.sol";
+import "./KyberNetworkProxy.sol";
+
 contract CapitalETH {
 
     address payable public owner;
@@ -10,11 +13,6 @@ contract CapitalETH {
 
     enum Status{ active, paused }
     uint totalSIPCount;
-
-    constructor() public {
-        owner = msg.sender;
-        processors[owner] = true;
-    }
 
     //Structure of the SIP Plan
     struct Plan {
@@ -37,10 +35,24 @@ contract CapitalETH {
     //Array of SIPs to store all the SIPs
     Plan[] public plans;
 
+    //Kyber Network Proxy contract address to swap tokens
+    KyberNetworkProxyInterface kyberNetworkProxyContract;
+
+    constructor() public {
+        owner = msg.sender;
+        processors[owner] = true;
+    }
+
     //Events
     event newSIPCreated(uint id, address indexed srcAccount);
     // SIPUpdated();
     // installmentReceived();
+    event tokensSwapped(
+        address indexed srcAccount,
+        ERC20 srcToken,
+        address indexed destAccount,
+        ERC20 destToken,
+        uint srcQty);
 
     function createSIP( address payable srcAccount,
                         address payable destAccount,
@@ -52,15 +64,7 @@ contract CapitalETH {
 
         uint planId = totalSIPCount++;
 
-        plans.push( Plan(planId,
-                         srcAccount,
-                         destAccount,
-                         srcToken,
-                         destToken,
-                         period,
-                         Status.active,
-                         amount,
-                         now));
+        plans.push(Plan(planId, srcAccount, destAccount, srcToken, destToken, period, Status.active, amount, now));
 
         userSIPCount[srcAccount]++;
         idToAddress[planId] = srcAccount;
@@ -69,15 +73,33 @@ contract CapitalETH {
         emit newSIPCreated(planId, srcAccount);
     }
 
-    // function processSIPs(uint[] memory planIDs) public {
+    // function processMultipleSIPs(uint[] memory planIDs) public {
 
     //     //Check if msg.sender is a valid processor
     //     require(processors[msg.sender], "Only valid processors can trigger installments");
 
     //     for(uint i = 0; i < planIDs.length; i++ ) {
-    //         // Plan memory userPlan = plans[i];
+    //         Plan memory userPlan = plans[i];
+    //         executeSwap( ERC20(userPlan.srcToken),
+    //                      userPlan.amount,
+    //                      ERC20(userPlan.destToken),
+    //                      userPlan.destAccount,
+    //                     1e18);
     //     }
     // }
+
+    function processSingleSIP(uint sipID) public {
+        // require(processors[msg.sender], "Only valid processors can trigger installments");
+
+        Plan memory userPlan = plans[sipID];
+        swapTokensUsingKyber(
+            userPlan.srcAccount,
+            ERC20(userPlan.srcToken),
+            userPlan.destAccount,
+            ERC20(userPlan.destToken),
+            userPlan.amount,
+            100 * 1e18);
+    }
 
     function pauseSIP(uint id) public returns (bool) {
         require(msg.sender == plans[id].srcAccount, "Not authorized to change SIP status");
@@ -86,6 +108,55 @@ contract CapitalETH {
 
         //Pause the status of SIP
         plans[id].status = Status.paused;
+    }
+
+/*
+*******************************************************************************
+* Kyber Network contract functions
+/******************************************************************************
+*/
+    function setKyberNetworkProxyContract(address contractAddress) public returns (bool) {
+        require(msg.sender == owner, "Not authorized to call this function");
+        kyberNetworkProxyContract = KyberNetworkProxyInterface(contractAddress);
+    }
+
+    function swapTokensUsingKyber(
+        address srcAccount,
+        ERC20 srcToken,
+        address destAccount,
+        ERC20 destToken,
+        uint srcQty,
+        uint maxDestAmount
+    ) public {
+
+        uint minConversionRate;
+        require(srcToken.transferFrom(
+            srcAccount,
+            address(this),
+            srcQty),
+            "Transfer of token from user account failed"
+        );
+
+        // Mitigate ERC20 Approve front-running attack, by initially setting allowance to 0
+        require(srcToken.approve(address(kyberNetworkProxyContract), 0), "Error approving allowance");
+        require(srcToken.approve(address(kyberNetworkProxyContract), srcQty), "Error approving allowance");
+
+        // Get the minimum conversion rate
+        (minConversionRate,) = kyberNetworkProxyContract.getExpectedRate(srcToken, destToken, srcQty);
+
+        // Swap the ERC20 token and send to destAddress
+        kyberNetworkProxyContract.trade(
+            srcToken,
+            srcQty,
+            destToken,
+            destAccount,
+            maxDestAmount,
+            minConversionRate,
+            owner
+        );
+
+        // Log the event
+        emit tokensSwapped(srcAccount, srcToken, destAccount, destToken, srcQty);
     }
 
 }

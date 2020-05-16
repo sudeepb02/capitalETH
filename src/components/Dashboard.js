5c606bfd0aeb2b0bdd51af67eceeb530e0b341b6
Sudeep Biswas <sudeepbiswas02@gmail.com>
2020-05-17 04:43:14 +0530

    Update dapp - Add Create SIP function - Add user-specific dashboard - Avoid loading dashboard and display message if user is not logged in - Update flow of dapp

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,40 +1,62 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {Web3Context} from './Web3Context';
-import CapitalETH from '../abis/CapitalETH.json';
-import SIPPlan from './SIPPlan';
+import React, { useContext, useState, useEffect } from 'react'
+import { Web3Context } from './Web3Context'
+import CapitalETH from '../abis/CapitalETH.json'
+import { SIPPlan, NewSIPPlan } from './SIPPlan'
 
 function Dashboard() {
+  const [web3, setWeb3, account, setAccount] = useContext(Web3Context)
+  const CAPITAL_ETH_ROPSTEN = '0x1a00C6e0255445869C1CF1C3651085b8C8a7E27e'
+  const CAPITAL_ETH_LOCAL = '0x0Eb8dCf3034d1fD26fd22E1BC787aCA7b4a51b87'
 
-    const [web3, setWeb3, account, setAccount] = useContext(Web3Context);
-    const CAPITAL_ETH_ROPSTEN = '0xCbEb765A8aD0dF1CF3Ba473EDb21991aa0464EFF';
+  const [plans, setPlans] = useState([])
+  const [isConnected, setIsConnected] = useState(false)
 
-    const [plans, setPlans] = useState({});
-
-    useEffect(() => {
-      fetchPlans();
-    }, []);
-
-    const fetchPlans = async () => {
-        if (account === '') {
-            const accounts = await web3.eth.getAccounts();
-            setAccount(accounts[0]);
-            console.log("Account", account);
-            console.log(account);
-        }
-
-        const capitalETHInstance = new web3.eth.Contract(CapitalETH.abi, CAPITAL_ETH_ROPSTEN);
-
-        const data = await capitalETHInstance.methods.plans(0).call();
-        console.log(data);
-        setPlans(data);
+  useEffect(() => {
+    if (account === '') {
+      //   alert('Please connect using a Web3 provider')
+      setIsConnected(false)
+    } else {
+      setIsConnected(true)
+      fetchPlans()
     }
-    return (
+  }, [account])
+
+  const fetchPlans = async () => {
+    const capitalETHInstance = new web3.eth.Contract(
+      CapitalETH.abi,
+      CAPITAL_ETH_ROPSTEN,
+    )
+
+    const userPlanIDs = await capitalETHInstance.methods
+      .getPlansByAddress(account)
+      .call()
+    console.log(userPlanIDs)
+
+    // setPlans(data)
+    // userPlanIDs.map
+    let userPlan
+    userPlanIDs.map(async (planID) => {
+      userPlan = await capitalETHInstance.methods.plans(planID).call()
+      console.log(userPlan)
+      setPlans((prevPlans) => [...prevPlans, userPlan])
+    })
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {isConnected ? (
         <div>
-            <h1>Dashboard</h1>
-            <SIPPlan data={plans}/>
-           
+          <NewSIPPlan />
+          {plans.map((plan) => (
+            <SIPPlan data={plan} key={plan.id} />
+          ))}
         </div>
-    );
+      ) : (
+        <p>Please connect to web3 to view your dashboard</p>
+      )}
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,47 @@
-import React, { useState, useEffect, useContext } from 'react';
-import {Link} from 'react-router-dom';
-import Web3 from "web3";
-import './Nav.css';
-import {Web3Context} from './Web3Context';
+import React, { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import Web3 from 'web3'
+import './Nav.css'
+import { Web3Context } from './Web3Context'
 
 function Nav() {
+  const [web3, setWeb3, account, setAccount] = useContext(Web3Context)
+  const [isConnected, setIsConnected] = useState(false)
 
-    const [web3, setWeb3] = useContext(Web3Context);
-    const [account, setAccount] = useState('');
+  const connect = async () => {
+    if (!isConnected) {
+      if (window.ethereum) {
+        console.log(web3)
+        await window.ethereum.enable()
+        console.log('Ethereum enabled')
+        const web3new = new Web3(window.ethereum)
+        setWeb3(web3new)
+        const accounts = await web3new.eth.getAccounts()
+        setAccount(accounts[0])
+        console.log(account)
+        setIsConnected(true)
+      }
+    }
+  }
 
-    const connect = async () => {
-        if (window.ethereum) {
-            await window.ethereum.enable();
-            console.log("Ethereum enabled");
-            const web3new = new Web3(window.ethereum);
-            setWeb3(web3new);
-            const accounts = await web3new.eth.getAccounts();
-            setAccount(accounts[0]);
-            console.log(account);
-        }
-    } 
-
-    return (
-        <nav>
-            <div className="logo">
-                <Link to="/">capitalETH</Link>
-            </div>
-            <ul className="nav-links">
-                <Link to='/dashboard'>
-                    <li>Dashboard</li>
-                </Link>
-                <Link to='/process'>
-                    <li>Process</li>
-                </Link>
-                <button onClick={connect}> 
-                    { account === '' ? "Connect" : "Connected"}
-                </button>
-            </ul>          
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="logo">
+        <Link to="/">capitalETH</Link>
+      </div>
+      <ul className="nav-links">
+        <Link to="/dashboard">
+          <li>Dashboard</li>
+        </Link>
+        <Link to="/process">
+          <li>Process</li>
+        </Link>
+        <button onClick={connect}>
+          {isConnected ? 'Connected' : 'Connect'}
+        </button>
+      </ul>
+    </nav>
+  )
 }
 
-export default Nav;
+export default Nav

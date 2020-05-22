2c1cca35ee2150ea73f57b33592632d25cc28373
Sudeep Biswas <sudeepbiswas02@gmail.com>
2020-05-23 04:27:00 +0530

    Add Activate/Pause functionality in dashboard

--- src/components/SIPPlan.js
+++ src/components/SIPPlan.js
@@ -2,15 +2,40 @@ import React, { useContext } from 'react'
 import Address from './Address'
 import { Web3Context } from './Web3Context'
 import { addressToTicker } from '../utils/tokenAddress'
+import { CAPITALETH_ROPSTEN } from '../utils/deployedAddress'
+import CapitalETH from '../abis/CapitalETH.json'
 import './SIPPlan.css'
 
 export function SIPPlan(props) {
-  const [web3] = useContext(Web3Context)
+  const [web3, setWeb3, account, setAccount] = useContext(Web3Context)
   const BN = web3.utils.BN
   const ONE_TOKEN = new BN(10).pow(new BN(18))
   const convertTokenAmount = (amount) => {
     let numberOfTokens = new BN(amount).div(ONE_TOKEN)
     return numberOfTokens.toString()
+  }
+
+  const capitalETHInstance = new web3.eth.Contract(
+    CapitalETH.abi,
+    CAPITALETH_ROPSTEN,
+  )
+
+  const pausePlan = async () => {
+    await capitalETHInstance.methods
+      .pauseSIP(props.data.id)
+      .send({ from: account })
+      .on('receipt', function () {
+        alert.success('Plan paused successfully!')
+      })
+  }
+
+  const resumePlan = async () => {
+    await capitalETHInstance.methods
+      .resumeSIP(props.data.id)
+      .send({ from: account })
+      .on('receipt', function () {
+        alert.success('Plan activated successfully!')
+      })
   }
 
   return (
@@ -31,6 +56,11 @@ export function SIPPlan(props) {
         {addressToTicker(props.data.srcToken)}
       </p>
       <p>Frequency: {props.data.frequency}</p>
+      {props.data.isActive ? (
+        <button onClick={pausePlan}>Pause Plan</button>
+      ) : (
+        <button onClick={resumePlan}>Activate Plan</button>
+      )}
     </div>
   )
 }
